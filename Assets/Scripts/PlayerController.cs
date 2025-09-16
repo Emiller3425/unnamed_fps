3fce898257404dee4782bec15e68be89fe79905d
Ethan <emiller3425@gmail.com>
2025-09-15 22:10:22 -0400

    player can walk around

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,12 @@
+using System;
+using System.Runtime.InteropServices;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.TextCore.Text;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
     public Camera playerCamera;
@@ -12,13 +19,40 @@ public class PlayerController : MonoBehaviour
     public Vector3 movementDirection = Vector3.zero;
     private float rotationX = 0;
     private bool canMove = true;
+
+    // new system
+    InputAction moveAction;
+    InputAction jumpAction;
+
     void Start()
     {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
 
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
     }
 
     void Update()
     {
+        // movement logic
+        if (canMove)
+        {
+            // Get WASD
+            Vector2 moveValue = moveAction.ReadValue<Vector2>() / 100f;
+            // Because W/S are the Y value of the Vector2, we apply it to the Z value of the Vector3 to simulate movement
+            movementDirection = new Vector3(moveValue.x, 0f, moveValue.y);
+            characterController.Move(movementDirection);
+
+            if (jumpAction.IsPressed())
+            {
+                // jump logic
+            }
+            // apply gravity
+
+            // lookaround logic
+        }
 
     }
 
