59b29645fe375ac25fa241899c84bf148bd59460
Ethan <emiller3425@gmail.com>
2025-09-16 22:08:09 -0400

    bullet and gun files, created template dirctory

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Runtime.InteropServices;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.TextCore.Text;
+
+public class Bullet : MonoBehaviour
+{
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+
+    void OnDestroy()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/TemplateScripts/templateMonobehavior.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Runtime.InteropServices;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.TextCore.Text;
+
+public class TemplateMonoBeheavior : MonoBehaviour
+{
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnDestroy()
+    {
+        
+    }
+
+}
