d5f5dfb8924f970f8e637827cc06c94b14140817
Ethan <emiller3425@gmail.com>
2025-09-17 23:14:05 -0400

    bullet and gun base classes

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,30 +1,60 @@
 using System;
+using System.Numerics;
 using System.Runtime.InteropServices;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.TextCore.Text;
+using UnityEngine.UIElements;
 
+[RequireComponent(typeof(BoxCollider), typeof(Rigidbody))]
 public class Bullet : MonoBehaviour
 {
+    private UnityEngine.Vector3 trajectory;
+    private float velocity;
+    private BoxCollider boxCollider ;
+    private Rigidbody rigidBody;
+
+    // Sets bullet movement variables
+    public void Shoot(UnityEngine.Vector3 direction, float speed)
+    {
+        trajectory = direction;
+        velocity = speed;
+    }
+
+    // sets colliders
     void Awake()
     {
-
+        boxCollider = GetComponent<BoxCollider>();
+        rigidBody = GetComponent<Rigidbody>();
     }
 
+    // sets bullet life to 3 seconds
     void Start()
     {
-
+        Destroy(gameObject, 3f);
     }
+
+    // updates bullet position
     void Update()
     {
+        if (velocity > 0)
+        {
+            transform.position += Time.deltaTime * velocity * trajectory;
+        }
+    }
 
+    // if collision occurs destryo bullet (will need to add damage logic later, potentially bullet holes in walls etc)
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
     }
 
     void OnDestroy()
     {
-        
+        // do nothing
     }
 
 }
--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,29 +4,65 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 using UnityEngine.TextCore.Text;
+
+
+public enum FiringType
+{
+    SEMIAUTO,
+    BURST,
+    FULLAUTO
+}
 
 public class Gun : MonoBehaviour
 {
+    public int maxAmmo = 30;
+    public int currentAmmo;
+    public int damage = 10;
+    public int fireRate = 2;
+    public FiringType firingType = FiringType.SEMIAUTO;
+    public GameObject bulletPrefab;
+    private InputAction shoot;
+    private float reloadBuffer;
     void Awake()
     {
-
+        shoot = InputSystem.actions.FindAction("Attack");
     }
 
     void Start()
     {
-
+        currentAmmo = maxAmmo;
     }
 
     void Update()
     {
+        shoot.started += context =>
+        {
+            if (context.interaction is TapInteraction && reloadBuffer == 0f)
+            {
+                ShootBullet();
+                currentAmmo--;
+            }
+        };
+    }
 
+    void ShootBullet()
+    {
+        GameObject bulletObject = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        Bullet bullet = bulletObject.GetComponent<Bullet>();
+        bullet.Shoot(transform.TransformDirection(Vector3.forward), 15f);
+    }
 
+    void Reload()
+    {
+        reloadBuffer = 2f;
+        currentAmmo = maxAmmo;
     }
 
     void OnDestroy()
     {
-        
+        // do nothing
     }
 
 }
