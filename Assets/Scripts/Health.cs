ddf87c9e07e932035af0f002a87c9cd0e93f8562
Ethan <emiller3425@gmail.com>
2025-09-27 17:08:05 -0400

    health script

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Runtime.InteropServices;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.TextCore.Text;
+
+public class TemplateMonoBeheavior : MonoBehaviour
+{
+    private float maxHealth = 100f;
+    private float currentHealth;
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void applyDamage(float damage)
+    {
+        currentHealth -= damage;
+    }
+    void applyHealing(float healing)
+    {
+        currentHealth += healing;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+}
